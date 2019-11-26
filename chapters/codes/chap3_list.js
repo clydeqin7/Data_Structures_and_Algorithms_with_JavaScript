ee608c99b626af4ac8f9be7e7ae98ca0b8cf8731
liaodanmin <liaodanmin@office.cgbchina>
2019-11-26 11:18:50 +0800

    List类数据完善，有错误待测试

--- chapters/codes/chap3_list.js
+++ chapters/codes/chap3_list.js
@@ -7,19 +7,19 @@ function List() {
     this.toString = toString;
     this.append = append;
     this.remove = remove;
-    // this.insert = insert;
-    // this.clear = clear;
-    // this.front = front;
-    // this.end = end;
-    // this.prev = prev;
-    // this.next = next;
-    // this.hasNext;
-    // this.hasPrev;
-    // this.length = length;
-    // this.currPos = currPos;
-    // this.moveTo = moveTo;
-    // this.getElement = getElement;
-    // this.contains = contains;
+    this.insert = insert;
+    this.clear = clear;
+    this.front = front;
+    this.end = end;
+    this.prev = prev;
+    this.next = next;
+    this.hasNext = hasNext;
+    this.hasPrev = hasNext;
+    this.length = length;
+    this.currPos = currPos;
+    this.moveTo = moveTo;
+    this.getElement = getElement;
+    this.contains = contains;
 }
 
 // 给列表添加元素
@@ -60,10 +60,114 @@ function toString() {
     return this.dataStore;
 }
 
+// 测试代码1
+// var names = new List();
+// names.append("andy");
+// names.append("bejanmin");
+// names.append("clyde");
+// console.log(names.toString());
+// names.remove("bejanmin");
+// console.log(names.toString());
+
+function insert(element, after) {
+    let insertPos = this.find(after)
+    if (insertPos > -1) {
+        this.dataStore.splice(insertPos, 0, element);
+        ++this.listSize;
+        return true;
+    }
+    return false;
+}
+
+function clear() {
+    // this.listSize = 0;
+    // this.dataStore = [];
+    // this.pos = 0
+    delete this.dataStore;
+    this.dataStore.length = 0;
+    this.listSize = this.pos = 0;
+}
+
+function contains(element) {
+    for (let i = 0; i < this.dataStore.length; i++) {
+        if (this.dataStore[i] === element) {
+            return true
+        }
+    }
+    return false
+}
+
+/* 遍历列表方法 start */
+function front() {
+    this.pos = 0
+}
+
+function end() {
+    this.pos = this.listSize - 1;
+}
+
+function prev() {
+    --this.pos;
+}
+
+function next() {
+    if(this.pos < this.listSize) {
+        ++this.pos;
+    }
+}
+
+function currPos() {
+    return this.pos;
+}
+
+function moveTo(position) {
+    this.pos = position;
+}
+
+function getElement() {
+    return this.dataStore[this.pos];
+}
+
+function hasNext() {
+    return this.pos < this.listSize;
+}
+
+function hasPrev() {
+    // return this.pos >= 0; //原文是这样
+    console.log(typeof this.pos, 'typeof')
+    return this.pos >= 0;
+}
+
+/* 遍历列表方法 end */
+
+// 测试数据2
 var names = new List();
-names.append("andy");
-names.append("bejanmin");
-names.append("clyde");
-console.log(names.toString());
-names.remove("bejanmin");
-console.log(names.toString());
+names.append("Clayton");
+names.append("Raymond");
+names.append("Cynthia");
+names.append("Jennifer");
+names.append("Bryan");
+names.append("Danny");
+// names.front();
+// console.log(names.getElement());
+// console.log(names.toString())
+
+// 迭代器访问列表
+// 正序遍历
+// for (names.front(); names.hasNext(); names.next()) {
+//     console.log(names.getElement())
+// }
+
+var i = 0;
+// console.log('listSize: ', names.listSize);
+// console.log("pos: ", names.pos);
+names.moveTo(-1);
+console.log('listSize: ', names.listSize);
+console.log("pos: ", names.pos);
+console.log(names.hasPrev())
+names.prev()
+console.log("now pos: ", names.pos)
+// 倒序遍历
+// for (names.end(); names.hasPrev(); names.prev()) {
+//     console.log(++i)
+// }
