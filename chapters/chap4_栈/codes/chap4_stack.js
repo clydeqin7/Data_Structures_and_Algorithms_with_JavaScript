002df7fcf296d9d27e45b85f8170884623e1bc9e
clydeqin7 <clydeqin7@foxmail.com>
2019-12-04 20:51:32 +0800

    新增1-8章笔记

--- chapters/chap4_栈/codes/chap4_stack.js
+++ chapters/chap4_栈/codes/chap4_stack.js
@@ -29,6 +29,87 @@ function clear() {
 }
 
 // 测试Stack类
-let s = new Stack();
-s.push("David");
-s.push("Raymond");
+// let s = new Stack();
+// s.push("David");
+// s.push("Raymond");
+// s.push("Bryan");
+// console.log("length: " + s.length());
+// console.log(s.peek());
+// let popped = s.pop();
+// console.log("The popped element is: " + popped);
+// console.log(s.peek());
+// s.push("Cynthia");
+// console.log(s.peek());
+// s.clear();
+// console.log("length: " + s.length());
+// console.log(s.peek());
+// s.push("Clayton");
+// console.log(s.peek());
+
+/*
+ * 数值间的相互转换
+ * 此算法只针对基数为 2 ~ 9 的情况
+ */
+function mulBase(num, base) {
+    let s = new Stack()
+    do {
+        s.push(num % base)
+        num = Math.floor(num /= base)
+    } while (num > 0)
+    let converted = ""
+    while (s.length() > 0) {
+        converted += s.pop()
+    }
+    return converted
+}
+
+// test
+// let num = 32
+// let base = 8
+// console.log('-----')
+// let xx = mulBase(num, base)
+// console.log(xx)
+
+// 回文
+function isPalindrome(word) {
+    let s = new Stack()
+    for (let i = 0; i < word.length; i++) {
+        s.push(word[i])
+    }
+    let converted = ""
+    while (s.length() > 0) {
+        converted += s.pop()
+    }
+    return converted === word
+}
+
+// let word = "hello"
+// console.log(isPalindrome(word))
+// word = "racecar"
+// console.log(isPalindrome(word))
+
+// 递归
+function factorial(n) {
+    if (n === 0) {
+        return 1
+    } else {
+        return n * factorial(n-1)
+    }
+}
+
+function fact(n) {
+    let s = new Stack()
+    while (n > 1) {
+        s.push(n--)
+    }
+    let product = 1
+    while (s.length() > 0) {
+        product *= s.pop()
+    }
+    return product
+}
+
+// console.log(factorial(5))
+// console.log(factorial(5))
+
+// 练习1
---
+++ chapters/chap7_字典/chap7_字典.md
@@ -0,0 +1,11 @@
+# 字典
+
+字典是一种以`键-值对`形式存储数据到数据结构。
+
+## Dictionary类
+
+Dictionary 类的基础是 Array 类。JS 中一切皆对象，数组也是对象。
+
+## Dictionary类的辅助方法
+
+## Dictionary类添加排序
